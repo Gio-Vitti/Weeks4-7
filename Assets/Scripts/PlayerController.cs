64ea93c0f66c235df6031715de7305243759e18d
Giovani Vitti Mamprin <vittimam@shernet.sheridancollege.ca>
2025-02-06 15:31:37 -0500

    Started coding gym 5

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public GameObject enableDisable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = transform.position;
+        if (pos.x > 2.8 && pos.y > 0.5)
+        {
+            
+        }
+    }
+    public void moveUp()
+    {
+        Vector2 pos = transform.position;
+        pos.y += speed;
+        transform.position = pos;
+    }
+
+   public void moveDown()
+    {
+        Vector2 pos = transform.position;
+        pos.y -= speed;
+        transform.position = pos;
+    }
+
+   public void moveLeft()
+    {
+        Vector2 pos = transform.position;
+        pos.x -= speed;
+        transform.position = pos;
+    }
+
+   public void moveRight()
+    {
+        Vector2 pos = transform.position;
+        pos.x += speed;
+        transform.position = pos;
+    }
+}
