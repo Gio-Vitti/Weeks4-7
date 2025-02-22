a7a370dbe6048f5fb6c74886aab705c5d3aae2f2
HOSHIKOSPC\Cliente <giovani.mamprin@icloud.com>
2025-02-22 18:53:47 -0500

    Added head and face sprites

---
+++ Assets/Assignment 2 Scripts/FaceDragAndDrop.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceDragAndDrop : MonoBehaviour
+{
+    //Variables:
+    //Detect when mouse is over object
+    bool mouseHovering;
+
+    //Follow mouse position
+    bool followMouse;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Assign Vectors to transform
+        Vector2 pos = transform.position;
+        Vector2 size = transform.localScale;
+        Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        //Check if the cursor is over the object and show transparent circle sprite
+        if (mouse.x >= pos.x - size.x / 2 && mouse.x <= pos.x + size.x / 2 && mouse.y >= pos.y - size.y / 2 && mouse.y <= pos.y + size.y / 2)
+        {
+            mouseHovering = true;
+            GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else
+        {
+            mouseHovering = false;
+            GetComponent<SpriteRenderer>().enabled = false;
+        }
+
+        //If the mouse is then clicked, make object follow the cursor
+        if (mouseHovering == true && Input.GetMouseButtonDown(0))
+        {
+            followMouse = true;
+        }
+
+        //When letting go of mouse, stop following the cursor
+        if (Input.GetMouseButtonUp(0))
+        {
+            followMouse = false;
+        }
+
+        //Boolean variable to prevent the object from stopping if the ouse goes too fast
+        if (followMouse == true)
+        {
+            transform.position = mouse;
+        }
+    }
+
+}
+
---
+++ Assets/Assignment 2 Scripts/SwitchHeads.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchHeads : MonoBehaviour
+{
+    public int head = 1;
+
+    public GameObject head1;
+    public GameObject head2;
+    public GameObject head3;
+    public GameObject head4;
+    public GameObject head5;
+    public GameObject head6;
+    public GameObject head7;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Reset head variable
+        if (head >= 8 || head == 0)
+        {
+            head = 1;
+        }
+
+        //Show corresponding head sprites:
+        //head 1
+        if (head == 1)
+        {
+            head1.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else { head1.GetComponent<SpriteRenderer>().enabled = false; }
+
+        //head 2
+        if (head == 2)
+        {
+            head2.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else { head2.GetComponent<SpriteRenderer>().enabled = false; }
+
+        //head 3
+        if (head == 3)
+        {
+            head3.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else { head3.GetComponent<SpriteRenderer>().enabled = false; }
+
+        //head 4
+        if (head == 4)
+        {
+            head4.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else { head4.GetComponent<SpriteRenderer>().enabled = false; }
+
+        //head 5
+        if (head == 5)
+        {
+            head5.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else { head5.GetComponent<SpriteRenderer>().enabled = false; }
+
+        //head 6
+        if (head == 6)
+        {
+            head6.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else { head6.GetComponent<SpriteRenderer>().enabled = false; }
+
+        //head 7
+        if (head == 7)
+        {
+            head7.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else { head7.GetComponent<SpriteRenderer>().enabled = false; }
+    }
+
+    //Change head variable with Right Arrow button 
+    public void headUp()
+    {
+        head += 1;
+    }
+
+    //Change head variable with Left Arrow button 
+    public void headDown()
+    {
+        head -= 1;
+    }
+}
+
