c11b84c51ff54b5cf81f6002b0be6a971c7c14f9
HOSHIKOSPC\Cliente <giovani.mamprin@icloud.com>
2025-02-22 12:54:17 -0500

    Added sprites and fixed problem with dragging multiple objects

--- Assets/Assignment 2 Scripts/DragAndDrop.cs
+++ Assets/Assignment 2 Scripts/DragAndDrop.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour
 {
+    //Variables:
+    //Detect when mouse is over object
     bool mouseHovering;
+
+    //Follow mouse position
+    bool followMouse;
 
     // Start is called before the first frame update
     void Start()
@@ -20,17 +25,31 @@ public class DragAndDrop : MonoBehaviour
         Vector2 size = transform.localScale;
         Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        //Check if the mouse is over the object
+        //Check if the cursor is over the object and show transparent circle sprite
         if (mouse.x >= pos.x - size.x / 2 && mouse.x <= pos.x + size.x / 2 && mouse.y >= pos.y - size.y / 2 && mouse.y <= pos.y + size.y / 2)
         {
             mouseHovering = true;
+            GetComponent<SpriteRenderer>().enabled = true;
         } else
         {
             mouseHovering = false;
+            GetComponent<SpriteRenderer>().enabled = false;
         }
 
-        //If the mouse is then clicked, mak object follow the cursor
-        if (mouseHovering == true && Input.GetMouseButton(0))
+        //If the mouse is then clicked, make object follow the cursor
+        if (mouseHovering == true && Input.GetMouseButtonDown(0))
+        {
+           followMouse = true;
+        }
+
+        //When letting go of mouse, stop following the cursor
+        if (Input.GetMouseButtonUp(0))
+        {
+            followMouse = false;
+        }
+
+        //Boolean variable to prevent the object from stopping if the ouse goes too fast
+        if (followMouse == true)
         {
             transform.position = mouse;
         }
